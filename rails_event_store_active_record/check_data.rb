7b1af2308b2fca99915e0e6db6debed6e4adad03
Paweł Pacana <pawel.pacana@gmail.com>
2020-02-25 01:14:00 +0100

    Move along.

--- rails_event_store_active_record/check_data.rb
+++ rails_event_store_active_record/check_data.rb
@@ -4,9 +4,13 @@ require_relative '../support/helpers/schema_helper'
 include SchemaHelper
 establish_database_connection
 
-event_names   = 200.times.map { |idx| "DummyEventKind%03d" % idx }
+event_names   = 20.times.map { |idx| "DummyEventKind%03d" % idx }
 event_classes = event_names.map { |name| Object.const_set(name, Class.new(RailsEventStore::Event)) }
 event_store   = RailsEventStore::Client.new
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+puts "event_store.read.of_type([DummyEventKind000]).count"
+
 require 'irb'
 binding.irb
+
--- rails_event_store_active_record/prepare_data.rb
+++ rails_event_store_active_record/prepare_data.rb
@@ -9,13 +9,13 @@ load_database_schema
 
 progress      = ProgressBar.create(total: 1000)
 event_store   = RailsEventStore::Client.new
-event_names   = 200.times.map { |idx| "DummyEventKind%03d" % idx }
+event_names   = 20.times.map { |idx| "DummyEventKind%03d" % idx }
 event_classes = event_names.map { |name| Object.const_set(name, Class.new(RailsEventStore::Event)) }
 
 
 1000.times do |idx|
   stream_name = "DummyStream%04d" % idx
-  events      = event_classes.map { |klass| klass.new }
+  events      = event_classes.flat_map { |klass| 10.times.map { klass.new } }
   event_store.append(events, stream_name: stream_name)
 
   progress.increment
