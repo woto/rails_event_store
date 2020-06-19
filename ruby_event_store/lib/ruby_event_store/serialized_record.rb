4561ef6a2e50ae8c34d540f335e3d25b35cd58df
Szymon Fiedler <szymon.fiedler@gmail.com>
2020-06-19 16:05:52 +0200

    Introduce serialized_data & serialized_metadata
    As an initial step to move serialization out of pipelines and moving it
    to repository. SerializedRecord is meant to become Record while keeping
    serialized form of data and metadata.

--- ruby_event_store/lib/ruby_event_store/serialized_record.rb
+++ ruby_event_store/lib/ruby_event_store/serialized_record.rb
@@ -3,16 +3,18 @@
 module RubyEventStore
   class SerializedRecord
     StringsRequired = Class.new(StandardError)
-    def initialize(event_id:, data:, metadata:, event_type:)
-      raise StringsRequired unless [event_id, event_type].all? { |v| v.instance_of?(String) }
-      @event_id   = event_id
-      @data       = data
-      @metadata   = metadata
-      @event_type = event_type
+    def initialize(event_id:, data:, metadata:, event_type:, serialized_data: '', serialized_metadata: '')
+      raise StringsRequired unless [event_id, event_type, serialized_data, serialized_metadata].all? { |v| v.instance_of?(String) }
+      @event_id            = event_id
+      @data                = data
+      @metadata            = metadata
+      @event_type          = event_type
+      @serialized_data     = serialized_data
+      @serialized_metadata = serialized_metadata
       freeze
     end
 
-    attr_reader :event_id, :data, :metadata, :event_type
+    attr_reader :event_id, :data, :metadata, :event_type, :serialized_data, :serialized_metadata
 
     BIG_VALUE = 0b110011100100000010010010110011101011110101010101001100111110011
     def hash
@@ -22,6 +24,8 @@ module RubyEventStore
         data,
         metadata,
         event_type,
+        serialized_data,
+        serialized_metadata,
       ].hash ^ BIG_VALUE
     end
 
@@ -30,15 +34,19 @@ module RubyEventStore
         other.event_id.eql?(event_id) &&
         other.data.eql?(data) &&
         other.metadata.eql?(metadata) &&
-        other.event_type.eql?(event_type)
+        other.event_type.eql?(event_type) &&
+        other.serialized_data.eql?(serialized_data) &&
+        other.serialized_metadata.eql?(serialized_metadata)
     end
 
     def to_h
       {
-        event_id: event_id,
-        data: data,
-        metadata: metadata,
-        event_type: event_type,
+        event_id:            event_id,
+        data:                data,
+        metadata:            metadata,
+        event_type:          event_type,
+        serialized_data:     serialized_data,
+        serialized_metadata: serialized_metadata,
       }
     end
 
--- ruby_event_store/spec/serialized_record_spec.rb
+++ ruby_event_store/spec/serialized_record_spec.rb
@@ -3,42 +3,69 @@ require 'spec_helper'
 module RubyEventStore
 
   RSpec.describe SerializedRecord do
-    let(:event_id)   { "event_id" }
-    let(:data)       { { foo: :bar } }
-    let(:metadata)   { { baz: :bax } }
-    let(:event_type) { "event_type" }
+    let(:event_id)            { "event_id" }
+    let(:data)                { { foo: :bar } }
+    let(:metadata)            { { baz: :bax } }
+    let(:event_type)          { "event_type" }
+    let(:serialized_data)     { "data" }
+    let(:serialized_metadata) { "metadata" }
 
     specify 'constructor accept all arguments and returns frozen instance' do
-      record = described_class.new(event_id: event_id, data: data, metadata: metadata, event_type: event_type)
-      expect(record.event_id).to   be event_id
-      expect(record.metadata).to   be metadata
-      expect(record.data).to       be data
-      expect(record.event_type).to be event_type
-      expect(record.frozen?).to    be true
+      record = SerializedRecord.new(
+        event_id:            event_id,
+        data:                data,
+        metadata:            metadata,
+        event_type:          event_type,
+        serialized_data:     serialized_data,
+        serialized_metadata: serialized_metadata
+      )
+
+      expect(record.event_id).to            be(event_id)
+      expect(record.metadata).to            be(metadata)
+      expect(record.data).to                be(data)
+      expect(record.event_type).to          be(event_type)
+      expect(record.serialized_data).to     be(serialized_data)
+      expect(record.serialized_metadata).to be(serialized_metadata)
+      expect(record.frozen?).to             be(true)
     end
 
     specify 'constructor raised SerializedRecord::StringsRequired when argument is not a String' do
-      [[1, 1, 1, 1],
-       [1, "string", "string", "string"],
-       ["string", "string", "string", 1]].each do |sample|
-        event_id, data, metadata, event_type = sample
+      [
+        [1, 1, 1, 1, 1, 1],
+        [1, "string", "string", "string", "string", "string"],
+        ["string", "string", "string", 1, 1, 1],
+        ["string", "string", "string", "string", 1, 1],
+        ["string", "string", "string", "string", "string", 1],
+        ["string", "string", "string", 1, "string", "string"],
+        ["string", "string", "string", "string", 1, "string"],
+      ].each do |sample|
+        event_id, data, metadata, event_type, serialized_data, serialized_metadata = sample
         expect do
-          described_class.new(event_id: event_id, data: data, metadata: metadata, event_type: event_type)
+          SerializedRecord.new(
+            event_id:            event_id,
+            data:                data,
+            metadata:            metadata,
+            event_type:          event_type,
+            serialized_data:     serialized_data,
+            serialized_metadata: serialized_metadata
+          )
         end.to raise_error SerializedRecord::StringsRequired
       end
     end
 
     specify "in-equality" do
       [
-        ["a", "a", "a", "a"],
-        ["b", "a", "a", "a"],
-        ["a", "b", "a", "a"],
-        ["a", "a", "b", "a"],
-        ["a", "a", "a", "b"],
+        ["a", "a", "a", "a", "a", "a"],
+        ["b", "a", "a", "a", "a", "a"],
+        ["a", "b", "a", "a", "a", "a"],
+        ["a", "a", "b", "a", "a", "a"],
+        ["a", "a", "a", "b", "a", "a"],
+        ["a", "a", "a", "a", "b", "a"],
+        ["a", "a", "a", "a", "a", "b"],
       ].permutation(2).each do |one, two|
-        a = SerializedRecord.new(event_id: one[0], data: one[1], metadata: one[2], event_type: one[3])
-        b = SerializedRecord.new(event_id: two[0], data: two[1], metadata: two[2], event_type: two[3])
-        c = Class.new(SerializedRecord).new(event_id: one[0], data: one[1], metadata: one[2], event_type: one[3])
+        a = SerializedRecord.new(event_id: one[0], data: one[1], metadata: one[2], event_type: one[3], serialized_data: one[4], serialized_metadata: one[5])
+        b = SerializedRecord.new(event_id: two[0], data: two[1], metadata: two[2], event_type: two[3], serialized_data: two[4], serialized_metadata: two[5])
+        c = Class.new(SerializedRecord).new(event_id: one[0], data: one[1], metadata: one[2], event_type: one[3], serialized_data: one[4], serialized_metadata: one[5])
         expect(a).not_to eq(b)
         expect(a).not_to eql(b)
         expect(a.hash).not_to eq(b.hash)
@@ -54,8 +81,8 @@ module RubyEventStore
     end
 
     specify "equality" do
-      a = SerializedRecord.new(event_id: "a", data: "b", metadata: "c", event_type: "d")
-      b = SerializedRecord.new(event_id: "a", data: "b", metadata: "c", event_type: "d")
+      a = SerializedRecord.new(event_id: "a", data: "b", metadata: "c", event_type: "d", serialized_data: "e", serialized_metadata: "f")
+      b = SerializedRecord.new(event_id: "a", data: "b", metadata: "c", event_type: "d", serialized_data: "e", serialized_metadata: "f")
       expect(a).to eq(b)
       expect(a).to eql(b)
       expect(a.hash).to eql(b.hash)
@@ -64,17 +91,19 @@ module RubyEventStore
     end
 
     specify "hash" do
-      a = SerializedRecord.new(event_id: "a", data: "b", metadata: "c", event_type: "d")
-      expect(a.hash).not_to eq([SerializedRecord, "a", "b", "c", "d"].hash)
+      a = SerializedRecord.new(event_id: "a", data: "b", metadata: "c", event_type: "d", serialized_data: "e", serialized_metadata: "f")
+      expect(a.hash).not_to eq([SerializedRecord, "a", "b", "c", "d", "e", "f"].hash)
     end
 
     specify "to_h" do
-      a = SerializedRecord.new(event_id: "a", data: "b", metadata: "c", event_type: "d")
+      a = SerializedRecord.new(event_id: "a", data: "b", metadata: "c", event_type: "d", serialized_data: "e", serialized_metadata: "f")
       expect(a.to_h).to eq({
         event_id: "a",
         data: "b",
         metadata: "c",
         event_type: "d",
+        serialized_data: "e",
+        serialized_metadata: "f"
       })
     end
 
@@ -82,6 +111,23 @@ module RubyEventStore
       expect do
         described_class.new
       end.to raise_error ArgumentError
+    end
+
+    specify 'default serialized_* values' do
+      record = SerializedRecord.new(
+        event_id:            event_id,
+        data:                data,
+        metadata:            metadata,
+        event_type:          event_type,
+        )
+
+      expect(record.event_id).to            be(event_id)
+      expect(record.metadata).to            be(metadata)
+      expect(record.data).to                be(data)
+      expect(record.event_type).to          be(event_type)
+      expect(record.serialized_data).to     eq('')
+      expect(record.serialized_metadata).to eq('')
+      expect(record.frozen?).to             be(true)
     end
   end
 end
